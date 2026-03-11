ba5a867102a78f0c1df6cc799fb78630574bad1d
Stefan Sträter <stefan.straeter@gmail.com>
2026-03-11 11:23:31 +0100

    feat: add MoveableObject class with position, size, and image loading

---
+++ models/moveable-object.class.js
@@ -0,0 +1,21 @@
+class MoveableObject {
+
+    x = 140;
+    y = 100;
+    img;
+    height = 150;
+    width = 400;
+
+    loadImage(path) {
+        this.img = new Image();
+        this.img.src = path;
+    };
+
+    moveRight() {
+
+    };
+
+    moveLeft() {
+
+    };
+}
