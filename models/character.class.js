4483e179334b7aa726455ac7f97ca49079c294ee
Stefan Sträter <stefan.straeter@gmail.com>
2026-03-12 09:26:14 +0100

    feat: add multiple animations for character

--- models/character.class.js
+++ models/character.class.js
@@ -1,12 +1,57 @@
 class Character extends MoveableObject {
 
-    x = 140;
+    x = 40;
     y = 200;
-    height = 250;
-    width = 250;
+    height = 235;
+    width = 235;
+
+    animations = {
+        idle: {
+            path: 'img/02_character_bud/Idle.png',
+            frames: 4
+        },
+        walk: {
+            path: 'img/02_character_bud/Walk.png',
+            frames: 6
+        },
+        attack: {
+            path: 'img/02_character_bud/Attack.png',
+            frames: 6
+        }
+    };
+
+    currentAnimation = 'null';
 
     constructor() {
         super();
-        this.loadImage('img/02_character_bud/Idle.png');
+        this.setAnimation('idle');
+    }
+
+    setAnimation(name) {
+
+        if (this.currentAnimation === name) return;
+
+        this.currentAnimation = name;
+
+        this.loadImage(this.animations[name].path);
+        this.frameCount = this.animations[name].frames;
+        this.currentFrame = 0;
     }
 }
+
+
+
+
+
+
+
+/*
+if (this.keyboard.RIGHT) {
+    this.character.setAnimation('walk');
+}
+
+if (this.keyboard.SPACE) {
+    this.character.setAnimation('attack');
+}
+
+*/
