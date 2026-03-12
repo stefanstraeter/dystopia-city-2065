8e4e16f8bd5cfaad952782cde00f3bcf14ab90d3
Stefan Sträter <stefan.straeter@gmail.com>
2026-03-12 09:27:08 +0100

    feat: updateAnimation() and draw()

--- models/moveable-object.class.js
+++ models/moveable-object.class.js
@@ -3,19 +3,50 @@ class MoveableObject {
     x = 140;
     y = 100;
     img;
-    height = 150;
-    width = 400;
+    height = 100;
+    width = 100;
+
+    frameCount = 1;
+    currentFrame = 0;
+    frameSpeed = 11;
+    frameCounter = 0;
 
     loadImage(path) {
         this.img = new Image();
         this.img.src = path;
-    };
+    }
+
+    updateAnimation() {
+        this.frameCounter++;
+        if (this.frameCounter > this.frameSpeed) {
+            this.frameCounter = 0;
+            this.currentFrame = (this.currentFrame + 1) % this.frameCount;
+        }
+    }
+
+    draw(ctx) {
+        if (!this.img.width) return;
+        if (!this.img.width) return;
+
+        const frameWidth = this.img.width / this.frameCount;
+        ctx.drawImage(
+            this.img,
+            this.currentFrame * frameWidth,
+            0,
+            frameWidth,
+            this.img.height,
+            this.x,
+            this.y,
+            this.width,
+            this.height
+        );
+    }
 
     moveRight() {
 
-    };
+    }
 
     moveLeft() {
 
-    };
+    }
 }
