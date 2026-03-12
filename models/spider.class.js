43b06c6843f27775d55027af8b040721b4a4bc60
Stefan Sträter <stefan.straeter@gmail.com>
2026-03-12 10:47:04 +0100

    Add Spider class with basic animation

--- models/spider.class.js
+++ models/spider.class.js
@@ -1,10 +1,41 @@
 class Spider extends MoveableObject {
 
+    y = 350;
+    height = 100;
+    width = 100;
+    animations = {
+        idle: {
+            path: 'img/02_character_bud/Idle.png',
+            frames: 4
+        },
+        walk: {
+            path: 'img/02_character_bud/Walk.png',
+            frames: 6
+        },
+        attack: {
+            path: 'img/03_enemies/spider/Attack.png',
+            frames: 12
+        }
+    };
+    currentAnimation = 'null';
+
+
     constructor() {
         super();
-        this.loadImage('img/03_enemies/spider/Idle.png')
+        this.setAnimation('attack');
+        this.x = 200 + Math.random() * 500;
+    }
 
-        this.x = 200;
-    };
+    setAnimation(name) {
+        if (this.currentAnimation === name) return;
+
+        this.currentAnimation = name;
+        this.loadImage(this.animations[name].path);
+        this.frameCount = this.animations[name].frames;
+        this.currentFrame = 0;
+    }
 
 }
+
+
+
