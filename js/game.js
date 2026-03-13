a7c02f355c97516b91b6092bd0eb771956c9b75e
Stefan Sträter <stefan.straeter@gmail.com>
2026-03-13 12:32:36 +0100

    feat: implement eventListener for key events

--- js/game.js
+++ js/game.js
@@ -1,11 +1,51 @@
 let canvas;
 let world;
+let keyboard = new Keyboard();
 
 function init() {
     canvas = document.getElementById('canvas');
-    world = new World(canvas);
+    world = new World(canvas, keyboard);
 
     console.log("My character is", world.character);
 };
+
+document.addEventListener('keydown', (event) => {
+    if (event.keyCode == 32) {
+        keyboard.KEY_SPACE = true;
+    }
+    if (event.keyCode == 37) {
+        keyboard.KEY_LEFT = true;
+    }
+    if (event.keyCode == 38) {
+        keyboard.KEY_UP = true;
+    }
+    if (event.keyCode == 39) {
+        keyboard.KEY_RIGHT = true;
+    }
+    if (event.keyCode == 40) {
+        keyboard.KEY_DOWN = true;
+    }
+    console.log(event);
+});
+
+document.addEventListener('keyup', (event) => {
+    if (event.keyCode == 32) {
+        keyboard.KEY_SPACE = false;
+    }
+    if (event.keyCode == 37) {
+        keyboard.KEY_LEFT = false;
+    }
+    if (event.keyCode == 38) {
+        keyboard.KEY_UP = false;
+    }
+    if (event.keyCode == 39) {
+        keyboard.KEY_RIGHT = false;
+    }
+    if (event.keyCode == 40) {
+        keyboard.KEY_DOWN = false;
+    }
+    console.log(event);
+});
+
 
 document.addEventListener('DOMContentLoaded', init);
