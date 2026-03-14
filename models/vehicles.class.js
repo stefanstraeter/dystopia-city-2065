b7b247197ffd2e2ce055d210597c1b8c7923e612
Stefan Sträter <stefan.straeter@gmail.com>
2026-03-14 19:12:33 +0100

    feat: implement Vehicles class for game objects

---
+++ models/vehicles.class.js
@@ -0,0 +1,15 @@
+class Police extends FlyingVehicle {
+    constructor(x, y, speed, direction, world, parallaxFactor) {
+        super('img/04_vehicles/police.png', x, y, speed, 60, 30, direction, world, parallaxFactor);
+    }
+}
+class Truck extends FlyingVehicle {
+    constructor(x, y, speed, direction, world, parallaxFactor) {
+        super('img/04_vehicles/truck.png', x, y, speed, 150, 60, direction, world, parallaxFactor);
+    }
+}
+class Drone extends FlyingVehicle {
+    constructor(x, y, speed, direction, world, parallaxFactor) {
+        super('img/04_vehicles/drone.png', x, y, speed, 40, 20, direction, world, parallaxFactor);
+    }
+}
