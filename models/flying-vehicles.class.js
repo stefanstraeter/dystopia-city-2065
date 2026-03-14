e05d218c40742eba3f09371c95f72528aaddf00a
Stefan Sträter <stefan.straeter@gmail.com>
2026-03-14 12:45:14 +0100

    feat: move() and handleRespwan() functionality

--- models/flying-vehicles.class.js
+++ models/flying-vehicles.class.js
@@ -1,6 +1,6 @@
 class FlyingVehicle extends MoveableObject {
 
-    constructor(imagePath, x = 800, y = 50, speed = 0.5, width = 60, height = 25) {
+    constructor(imagePath, x = 800, y = 50, speed = 0.5, width = 60, height = 25, direction = -1) {
         super();
         this.loadImage(imagePath);
         this.x = x;
@@ -8,17 +8,28 @@ class FlyingVehicle extends MoveableObject {
         this.speed = speed;
         this.width = width;
         this.height = height;
+        this.direction = direction;
     }
 
     move() {
-        this.x -= this.speed;
-        if (this.x < -this.width) {
+        this.x += this.speed * this.direction;
+        this.isMirrored = this.direction === 1;
+        this.handleRespawn();
+    }
+
+    handleRespawn() {
+        if (this.direction === -1 && this.x < -this.width) {
             this.x = 900;
+        }
+        if (this.direction === 1 && this.x > 900) {
+            this.x = -this.width;
         }
     }
 
     updateState() {
         this.move();
-        super.animate();
+        if (this.frameCount > 1) {
+            super.animate();
+        }
     }
-}
+} 1
