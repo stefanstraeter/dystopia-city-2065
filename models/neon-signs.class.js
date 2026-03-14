2892cb3bfb57e765fa1775a7b2790396e0f4a14e
Stefan Sträter <stefan.straeter@gmail.com>
2026-03-14 19:11:18 +0100

    chore: remove comments

--- models/neon-signs.class.js
+++ models/neon-signs.class.js
@@ -1,31 +1,17 @@
 class NeonSign extends MoveableObject {
 
-    // Du kannst hier Standard-Werte festlegen
-    // oder sie später im Constructor übergeben.
-
     constructor(path, x, y, width, height, frames, speed) {
         super();
         this.x = x;
-        this.y = y; // Position auf dem Canvas
+        this.y = y;
         this.width = width;
         this.height = height;
-
-        // Da die Schilder feststehen, brauchen sie keine Schwerkraft.
-        // Aber sie brauchen die Animations-Logik.
-
-        this.loadImage(path); // Lädt das erste Bild/Spritesheet
-
-        // Animations-Einstellungen
-        this.frameCount = frames; // Wie viele Einzelbilder hat das Spritesheet?
-        this.frameSpeed = speed;   // Wie schnell soll es blinken? (höher = langsamer)
-
-        // Da Schilder sich nicht bewegen, reicht es, die Animation zu starten.
-        // 'animate()' von MoveableObject erledigt den Rest.
+        this.frameCount = frames;
+        this.frameSpeed = speed;
+        this.loadImage(path);
     }
 
-    // Wir überschreiben updateState, damit das Schild nur animiert wird,
-    // aber sich nicht bewegt.
     updateState() {
-        this.animate(); // Ruft super.animate() auf, um die Frames durchzugehen
+        this.animate();
     }
 }
