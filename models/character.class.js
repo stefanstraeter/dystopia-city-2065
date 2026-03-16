0d43820308d6e245e1266c24a17e71641bb5a0dc
Stefan Sträter <stefan.straeter@gmail.com>
2026-03-16 16:06:36 +0100

    character update: fixed speed, added offsets and implemented death/hurt animations

--- models/character.class.js
+++ models/character.class.js
@@ -1,10 +1,10 @@
 class Character extends MoveableObject {
 
     animations = {
-        idle: { path: 'img/02_character_bud/Idle.png', frames: 5, speed: 12 },
-        walk: { path: 'img/02_character_bud/Walk.png', frames: 4, speed: 6 },
+        idle: { path: 'img/02_character_bud/Idle.png', frames: 5, speed: 4 },
+        walk: { path: 'img/02_character_bud/Walk.png', frames: 4, speed: 3 },
         attack: { path: 'img/02_character_bud/Attack.png', frames: 6, speed: 6 },
-        jump: { path: 'img/02_character_bud/Jump.png', frames: 7, speed: 8 },
+        jump: { path: 'img/02_character_bud/Jump.png', frames: 7, speed: 3 },
         hurt: { path: 'img/02_character_bud/Hurt.png', frames: 2, speed: 4 },
         death: { path: 'img/02_character_bud/Death.png', frames: 7, speed: 2 }
     };
@@ -15,11 +15,14 @@ class Character extends MoveableObject {
         this.width = width;
         this.height = height;
         this.speed = speed;
+        this.footOffset = 0;
+        this.offset = { top: 40, bottom: 80, left: 80, right: 80 };
         this.applyGravity();
         this.playAnimation('idle');
     }
 
     updateState() {
+        this.applyGravity();
         super.animate();
         this.handleMovement();
     }
@@ -44,7 +47,11 @@ class Character extends MoveableObject {
     }
 
     updateAnimation() {
-        if (this.isAboveGround()) {
+        if (this.isDead()) {
+            this.playAnimation('death');
+        } else if (this.isHurt()) {
+            this.playAnimation('hurt');
+        } else if (this.isAboveGround()) {
             this.playAnimation('jump');
         } else if (this.world.keyboard.KEY_RIGHT || this.world.keyboard.KEY_LEFT) {
             this.playAnimation('walk');
