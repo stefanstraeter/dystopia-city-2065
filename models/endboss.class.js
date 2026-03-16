2a00fc90b7093c67e376e9ca7214a76632046932
Stefan Sträter <stefan.straeter@gmail.com>
2026-03-16 16:07:07 +0100

    endboss update: fixed speed, added offsets and implemented patrol animations

--- models/endboss.class.js
+++ models/endboss.class.js
@@ -4,19 +4,41 @@ class Endboss extends MoveableObject {
         idle: { path: 'img/03_enemies/endboss/Idle.png', frames: 6 },
     };
 
+    startX;
+    distance = 150;
+    walkingRight = false;
+
     constructor(x, width, height, speed) {
-        super();
+        super(x, width, height, speed);
         this.x = x;
+        this.startX = x;
         this.width = width;
         this.height = height;
         this.speed = speed;
-        this.offset.bottom = -20;
-        this.isMirrored = true;
+        this.footOffset = -10;
+        this.offset = { top: 50, bottom: 50, left: 60, right: 60 };
+
         this.applyGravity();
         this.playAnimation('idle');
     }
 
     updateState() {
         super.animate();
+        this.applyGravity();
+        this.patrol();
+    }
+
+    patrol() {
+        if (this.walkingRight) {
+            this.moveRight();
+            if (this.x > this.startX + this.distance) {
+                this.walkingRight = false;
+            }
+        } else {
+            this.moveLeft();
+            if (this.x < this.startX - this.distance) {
+                this.walkingRight = true;
+            }
+        }
     }
 }
