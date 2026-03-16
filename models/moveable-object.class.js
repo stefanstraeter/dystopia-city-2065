8eeabbd71fad02b7c537356e58f2808754d38c5b
Stefan Sträter <stefan.straeter@gmail.com>
2026-03-16 19:08:33 +0100

    refactor: move drawing logic from MoveableObject to DrawableObject

--- models/moveable-object.class.js
+++ models/moveable-object.class.js
@@ -1,28 +1,25 @@
-class MoveableObject {
+class MoveableObject extends DrawableObject {
 
-    x = 140;
-    y = 100;
-    height = 100;
-    width = 100;
-    offset = { top: 0, bottom: 0, left: 0, right: 0 };
-    footOffset = 0;
     speed = 0.15;
     speedY = 0;
     acceleration = 1;
     energy = 100;
     lastHit = 0;
-    img;
     animations = {};
     currentAnimation = null;
-    currentFrame = 0;
-    frameCount = 1;
     frameCounter = 0;
     frameSpeed = 11;
     isMirrored = false;
+    offset = { top: 0, bottom: 0, left: 0, right: 0 };
+    footOffset = 0;
+    damage = 10;
 
-    loadImage(path) {
-        this.img = new Image();
-        this.img.src = path;
+    draw(ctx) {
+        if (!this.img || !this.img.complete) return;
+        this.flipImage(ctx);
+        this.drawImage(ctx);
+        this.drawHitbox(ctx);
+        this.flipImageBack(ctx);
     }
 
     animate() {
@@ -32,10 +29,8 @@ class MoveableObject {
             this.frameCounter = 0;
 
             let isDeathEnd = this.currentAnimation === 'death' && this.currentFrame >= this.frameCount - 1;
-            let isJumpEnd = this.currentAnimation === 'jump' && this.currentFrame >= this.frameCount - 1;
 
             if (isDeathEnd) {
-            } else if (isJumpEnd) {
             } else {
                 this.currentFrame = (this.currentFrame + 1) % this.frameCount;
             }
@@ -56,20 +51,6 @@ class MoveableObject {
         this.frameCounter = 0;
     }
 
-    moveLeft() {
-        this.x -= this.speed;
-        this.isMirrored = true;
-    }
-
-    moveRight() {
-        this.x += this.speed;
-        this.isMirrored = false;
-    }
-
-    jump() {
-        this.speedY = 21;
-    }
-
     applyGravity() {
         if (this.isAboveGround() || this.speedY > 0) {
             this.y -= this.speedY;
@@ -87,27 +68,18 @@ class MoveableObject {
         return this.y < (this.world.groundLevel - this.height + this.footOffset);
     }
 
-    draw(ctx) {
-        if (!this.img || !this.img.complete) return;
-
-        this.flipImage(ctx);
-        this.drawImage(ctx);
-        this.drawHitbox(ctx);
-        this.flipImageBack(ctx);
+    moveLeft() {
+        this.x -= this.speed;
+        this.isMirrored = true;
     }
 
-    drawImage(ctx) {
-        const frameWidth = this.img.width / this.frameCount;
-        ctx.drawImage(
-            this.img,
-            this.currentFrame * frameWidth, 0,
-            frameWidth,
-            this.img.height,
-            this.x,
-            this.y,
-            this.width,
-            this.height
-        );
+    moveRight() {
+        this.x += this.speed;
+        this.isMirrored = false;
+    }
+
+    jump() {
+        this.speedY = 21;
     }
 
     flipImage(ctx) {
@@ -136,7 +108,7 @@ class MoveableObject {
     hit() {
         if (this.isHurt()) return;
 
-        this.energy -= 10;
+        this.energy -= this.damage;
         if (this.energy < 0) {
             this.energy = 0;
         } else {
@@ -146,29 +118,12 @@ class MoveableObject {
 
     isHurt() {
         let timepassed = (new Date().getTime() - this.lastHit) / 1000;
-        return timepassed < 0.5;
+        return timepassed < 0.8;
     }
 
     isDead() {
         return this.energy == 0;
     }
-
-    drawHitbox(ctx) {
-        if (this instanceof Character || this instanceof Spider || this instanceof Endboss) {
-            ctx.beginPath();
-            ctx.lineWidth = "2";
-            ctx.strokeStyle = "red";
-            ctx.rect(
-                this.x + this.offset.left,
-                this.y + this.offset.top,
-                this.width - this.offset.left - this.offset.right,
-                this.height - this.offset.top - this.offset.bottom
-            );
-            ctx.stroke();
-        }
-    }
-
-
 }
 
 
