e1907ab217172c4d66eafc1a0191ac0b8bcf19ed
Stefan Sträter <stefan.straeter@gmail.com>
2026-03-16 19:09:22 +0100

    chore: change order

--- models/neon-signs.class.js
+++ models/neon-signs.class.js
@@ -1,14 +1,14 @@
-class NeonSign extends MoveableObject {
+class NeonSign extends DrawableObject {
 
     constructor(path, x, y, width, height, frames, speed) {
         super();
+        this.loadImage(path);
         this.x = x;
         this.y = y;
         this.width = width;
         this.height = height;
         this.frameCount = frames;
         this.frameSpeed = speed;
-        this.loadImage(path);
     }
 
     updateState() {
