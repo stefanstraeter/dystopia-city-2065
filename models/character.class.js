940a47204e4a257b784e99d2b9fdc23f351dc319
Stefan Sträter <stefan.straeter@gmail.com>
2026-03-17 19:12:10 +0100

    refactor: adjust URLs to match new folder structure

--- models/character.class.js
+++ models/character.class.js
@@ -1,13 +1,13 @@
 class Character extends MoveableObject {
 
     animations = {
-        idle: { path: 'img/02_character_bud/Idle.png', frames: 5, speed: 4 },
-        walk: { path: 'img/02_character_bud/Walk.png', frames: 4, speed: 3 },
-        attackGun: { path: 'img/02_character_bud/Attack_gun.png', frames: 7, speed: 6 },
-        attackBomb: { path: 'img/02_character_bud/Attack_bomb.png', frames: 6, speed: 4 },
-        jump: { path: 'img/02_character_bud/Jump.png', frames: 7, speed: 3 },
-        hurt: { path: 'img/02_character_bud/Hurt.png', frames: 2, speed: 4 },
-        death: { path: 'img/02_character_bud/Death.png', frames: 7, speed: 2 }
+        idle: { path: 'assets/img/02_character_bud/Idle.png', frames: 5, speed: 4 },
+        walk: { path: 'assets/img/02_character_bud/Walk.png', frames: 4, speed: 3 },
+        attackGun: { path: 'assets/img/02_character_bud/Attack_gun.png', frames: 7, speed: 6 },
+        attackRocket: { path: 'assets/img/02_character_bud/Attack_rocket.png', frames: 6, speed: 4 },
+        jump: { path: 'assets/img/02_character_bud/Jump.png', frames: 7, speed: 3 },
+        hurt: { path: 'assets/img/02_character_bud/Hurt.png', frames: 2, speed: 4 },
+        death: { path: 'assets/img/02_character_bud/Death.png', frames: 7, speed: 2 }
     };
 
     lastShootTime = 0;
@@ -19,7 +19,7 @@ class Character extends MoveableObject {
         this.height = height;
         this.speed = speed; 6
         this.footOffset = 0;
-        this.offset = { top: 40, bottom: 80, left: 80, right: 80 };
+        this.offset = { top: 40, bottom: 80, left: 100, right: 100 };
         this.energy = 100;
         this.applyGravity();
         this.playAnimation('idle');
@@ -46,18 +46,16 @@ class Character extends MoveableObject {
         if (this.world.keyboard.KEY_RIGHT && this.x < (this.world.level.level_end_x - this.width)) {
             this.moveRight();
         }
-
         if (this.world.keyboard.KEY_LEFT && this.x > 0) {
             this.moveLeft();
         }
-
         if (this.world.keyboard.KEY_UP && !this.isAboveGround()) {
             this.jump();
         }
-
         if (this.world.keyboard.KEY_SPACE) {
             this.shoot();
         }
+
     }
 
     updateAnimation() {
@@ -70,7 +68,7 @@ class Character extends MoveableObject {
         } else if (this.world.keyboard.KEY_SPACE) {
             this.playAnimation('attackGun');
         } else if (this.world.keyboard.KEY_X) {
-            this.playAnimation('attackBomb');
+            this.playAnimation('attackRocket');
         } else if (this.isAboveGround()) {
             this.playAnimation('jump');
         } else if (this.world.keyboard.KEY_RIGHT || this.world.keyboard.KEY_LEFT) {
@@ -85,18 +83,18 @@ class Character extends MoveableObject {
         if (currentTime - this.lastShootTime > 400) {
             this.playAnimation('attackGun');
             let plasmaX;
-            let plasmaY = this.y + 70;
+            let plasmaY = this.y + 80;
 
             if (this.isMirrored) {
-                plasmaX = this.x + 40;
+                plasmaX = this.x + 20;
             } else {
-                plasmaX = this.x + 180;
+                plasmaX = this.x + 140;
             }
+
             let newPlasma = new Plasma(plasmaX, plasmaY, this.isMirrored);
             this.world.throwableObjects.push(newPlasma);
             this.lastShootTime = currentTime;
         }
     }
-
 
 }
