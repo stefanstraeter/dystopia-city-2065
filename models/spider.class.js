4cc1bb3cd2ceb77b34984fafe661ee4e3091a619
Stefan Sträter <stefan.straeter@gmail.com>
2026-03-17 19:15:08 +0100

    refactor: restructure Spider class and updateState, handleDeath, handleAi, pursueCharacter handling

--- models/spider.class.js
+++ models/spider.class.js
@@ -1,9 +1,14 @@
 class Spider extends MoveableObject {
 
+    AGGRO_RANGE = 200;
+    CHASE_SPEED = 2;
+    NORMAL_SPEED = 1;
+
     animations = {
-        idle: { path: 'img/03_enemies/spider/Idle.png', frames: 4, speed: 6 },
-        walk: { path: 'img/03_enemies/spider/Walk.png', frames: 12, speed: 2 },
-        attack: { path: 'img/03_enemies/spider/Attack.png', frames: 12, speed: 6 }
+        idle: { path: 'assets/img/03_enemies/spider/Idle.png', frames: 4, speed: 6 },
+        walk: { path: 'assets/img/03_enemies/spider/Walk.png', frames: 12, speed: 2 },
+        attack: { path: 'assets/img/03_enemies/spider/Attack.png', frames: 12, speed: 6 },
+        death: { path: 'assets/img/03_enemies/spider/Death.png', frames: 9, speed: 6 }
     };
 
     constructor(x, width, height, speed) {
@@ -12,17 +17,54 @@ class Spider extends MoveableObject {
         this.width = width;
         this.height = height;
         this.speed = speed;
-        this.footOffset = -30;
-        this.offset = { top: 5, bottom: 30, left: 20, right: 20 };
+        this.footOffset = -50;
+        this.offset = { top: 5, bottom: 30, left: 40, right: 40 };
+        this.energy = 50;
         this.applyGravity();
         this.playAnimation('walk');
     }
 
-
     updateState() {
+        if (this.isDead()) {
+            this.handleDeath();
+            return;
+        }
+
+        this.handleAI();
         super.animate();
-        this.moveLeft();
         this.applyGravity();
     }
-}
 
+    handleDeath() {
+        this.playAnimation('death');
+        super.animate();
+        this.y += 0.5;
+
+        if (this.currentFrame >= this.frameCount - 1) {
+            this.isFinished = true;
+        }
+    }
+
+    handleAI() {
+        if (!this.world || !this.world.character) return;
+
+        let distance = Math.abs(this.x - this.world.character.x);
+
+        if (this.isColliding(this.world.character)) {
+            this.playAnimation('attack');
+        } else {
+            this.pursueCharacter(distance);
+        }
+    }
+
+    pursueCharacter(distance) {
+        if (distance < this.AGGRO_RANGE) {
+            this.speed = this.CHASE_SPEED;
+        } else {
+            this.speed = this.NORMAL_SPEED;
+        }
+
+        this.moveLeft();
+        this.playAnimation('walk');
+    }
+}
