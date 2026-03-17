51ac346c073b148ef847b45232d153dd1b34525d
Stefan Sträter <stefan.straeter@gmail.com>
2026-03-17 19:12:19 +0100

    refactor: adjust URLs to match new folder structure

--- models/items.class.js
+++ models/items.class.js
@@ -1,14 +1,14 @@
 class PlasmaCore extends CollectableObject {
     value = 25;
     constructor(x, y) {
-        super('img/08_collectables/plasma_core.png', x, y, 50, 50, 4, 8);
+        super('assets/img/08_collectables/plasma_core.png', x, y, 50, 50, 4, 8);
     }
 }
 
 class PowerCell extends CollectableObject {
     value = 25;
     constructor(x, y) {
-        super('img/08_collectables/power_cell.png', x, y, 50, 50, 4, 8);
+        super('assets/img/08_collectables/power_cell.png', x, y, 50, 50, 4, 8);
     }
 }
 
@@ -16,6 +16,6 @@ class PowerCell extends CollectableObject {
 class RocketAmmo extends CollectableObject {
     value = 25;
     constructor(x, y) {
-        super('img/08_collectables/rocket_ammo.png', x, y, 50, 50, 4, 8);
+        super('assets/img/08_collectables/rocket_ammo.png', x, y, 50, 50, 4, 8);
     }
 }
