e9c109fbda1ba1c1652795e8304744ab91a58506
Stefan Sträter <stefan.straeter@gmail.com>
2026-03-17 14:10:58 +0100

    chore: change animation count for attack

--- models/character.class.js
+++ models/character.class.js
@@ -3,7 +3,7 @@ class Character extends MoveableObject {
     animations = {
         idle: { path: 'img/02_character_bud/Idle.png', frames: 5, speed: 4 },
         walk: { path: 'img/02_character_bud/Walk.png', frames: 4, speed: 3 },
-        attack: { path: 'img/02_character_bud/Attack.png', frames: 6, speed: 6 },
+        attack: { path: 'img/02_character_bud/Attack_gun.png', frames: 7, speed: 6 },
         jump: { path: 'img/02_character_bud/Jump.png', frames: 7, speed: 3 },
         hurt: { path: 'img/02_character_bud/Hurt.png', frames: 2, speed: 4 },
         death: { path: 'img/02_character_bud/Death.png', frames: 7, speed: 2 }
