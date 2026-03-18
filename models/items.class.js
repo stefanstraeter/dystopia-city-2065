611d8b057a500f12821bf617b15b0ef0cfbe6bac
Stefan Sträter <stefan.straeter@gmail.com>
2026-03-18 13:13:10 +0100

    refactor: add collectable objects to items

--- models/items.class.js
+++ models/items.class.js
@@ -1,21 +1,42 @@
+class CollectableObject extends MoveableObject {
+
+    constructor(path, x, y, width = 50, height = 50, frames = 4, speed = 8) {
+        super();
+        this.loadImage(path);
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        this.frameCount = frames;
+        this.frameSpeed = speed;
+        this.offset = { top: 5, bottom: 5, left: 5, right: 5 };
+    }
+
+    updateState() {
+        if (this.frameCount > 1) {
+            this.animate();
+        }
+    }
+}
+
+
 class PlasmaCore extends CollectableObject {
     value = 25;
     constructor(x, y) {
-        super('assets/img/08_collectables/plasma_core.png', x, y, 50, 50, 4, 8);
+        super('assets/img/08_collectables/plasma_core.png', x, y);
     }
 }
 
 class PowerCell extends CollectableObject {
     value = 25;
     constructor(x, y) {
-        super('assets/img/08_collectables/power_cell.png', x, y, 50, 50, 4, 8);
+        super('assets/img/08_collectables/power_cell.png', x, y);
     }
 }
 
-
 class RocketAmmo extends CollectableObject {
-    value = 25;
+    value = 10;
     constructor(x, y) {
-        super('assets/img/08_collectables/rocket_ammo.png', x, y, 50, 50, 4, 8);
+        super('assets/img/08_collectables/rocket_ammo.png', x, y);
     }
 }
