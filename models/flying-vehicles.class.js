bae6443858d4851cd58d495890a363afb6dd5512
Stefan Sträter <stefan.straeter@gmail.com>
2026-03-18 17:25:20 +0100

    refactor: consolidate vehicle classes into a single flying-vehicles.js file

--- models/flying-vehicles.class.js
+++ models/flying-vehicles.class.js
@@ -38,3 +38,21 @@ class FlyingVehicle extends MoveableObject {
         }
     }
 }
+
+class Police extends FlyingVehicle {
+    constructor(x, y, speed, direction, world, parallaxFactor) {
+        super('assets/img/04_vehicles/police.png', x, y, speed, 60, 30, direction, world, parallaxFactor);
+    }
+}
+
+class Truck extends FlyingVehicle {
+    constructor(x, y, speed, direction, world, parallaxFactor) {
+        super('assets/img/04_vehicles/truck.png', x, y, speed, 120, 40, direction, world, parallaxFactor);
+    }
+}
+
+class Drone extends FlyingVehicle {
+    constructor(x, y, speed, direction, world, parallaxFactor) {
+        super('assets/img/04_vehicles/drone.png', x, y, speed, 40, 20, direction, world, parallaxFactor);
+    }
+}
