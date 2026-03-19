38e452e2e37f821e6a480facf91246c2595b46d4
Stefan Sträter <stefan.straeter@gmail.com>
2026-03-19 19:03:36 +0100

    reafctor clean up world

--- models/world.class.js
+++ models/world.class.js
@@ -1,137 +1,147 @@
 class World {
-    canvas;
-    ctx;
-    keyboard;
+    canvas; ctx; keyboard;
     camera_x = 0;
     isShaking = false;
     groundLevel = 490;
-    character;
-    level;
-    enemies;
-    flyingVehicles;
-    backgroundLayers;
-    neonSigns;
+    character; level; enemies;
     throwableObjects = [];
-    healthBar;
-    plasmaBar;
-    rocketBar;
+    gameStarted = false;
+    showHelp = false;
 
     constructor(canvas, keyboard) {
         this.ctx = canvas.getContext('2d');
         this.canvas = canvas;
         this.keyboard = keyboard;
-
         this.uiManager = new UIManager(this.ctx, this.canvas);
-        this.gameStarted = false;
-
+        this.collisionManager = new CollisionManager(this);
         this.level = level1;
+        this.audioManager = new AudioManager();
 
-        this.healthBar = this.level.UIElements[0];
-        this.plasmaBar = this.level.UIElements[1];
-        this.ammoBar = this.level.UIElements[2];
-
-        this.character = new Character(100, 250, 250, 5);
-        this.character.world = this;
-
-        this.ammoBar.setPercentage(this.character.ammo);
-        this.plasmaBar.setPercentage(this.character.plasma);
-
-        this.enemies = this.level.enemies;
-        this.flyingVehicles = this.level.vehicles;
-        this.backgroundLayers = this.level.backgrounds;
-        this.neonSigns = this.level.neonSigns;
-
-        this.showHelp = false;
-        this.hKeyPressed = false;
-
-        this.setWorld();
+        this.initLevel();
         this.draw();
         this.run();
     }
 
-    setWorld() {
+    initLevel() {
+        this.healthBar = this.level.UIElements[0];
+        this.plasmaBar = this.level.UIElements[1];
+        this.ammoBar = this.level.UIElements[2];
+        this.character = new Character(100, 250, 250, 5);
         this.character.world = this;
         this.character.y = this.groundLevel - this.character.height;
-        this.enemies.forEach(enemy => {
-            enemy.world = this;
-            if (!(enemy instanceof SentryDrone)) {
-                enemy.y = this.groundLevel - enemy.height;
-            }
-        });
-        this.neonSigns.forEach(sign => sign.world = this);
-        this.flyingVehicles.background.forEach(vehicle => vehicle.world = this);
-        this.flyingVehicles.midground.forEach(vehicle => vehicle.world = this);
+        this.spawnTraffic(15);
+        this.applyWorldToObjects();
     }
 
+    applyWorldToObjects() {
+        const allObjects = [
+            ...this.level.enemies,
+            ...this.level.neonSigns,
+            ...this.level.collectableItems,
+            ...this.level.vehicles.background,
+            ...this.level.vehicles.midground
+        ];
+
+        allObjects.forEach(obj => {
+            obj.world = this;
+
+            const isFlying = obj instanceof FlyingVehicle || obj instanceof SentryDrone;
+            const isItem = obj instanceof CollectableObject;
+
+            if (obj instanceof MoveableObject && !isFlying && !isItem) {
+                obj.y = this.groundLevel - obj.height;
+            }
+        });
+    }
+
+    run() {
+        setInterval(() => {
+            const isDying = this.character.energy <= 0 && !this.character.isDeadAnimationFinished();
+            const canUpdate = this.gameStarted && !this.showHelp && (this.character.energy > 0 || isDying);
+            if (canUpdate) {
+                this.updateAllObjects();
+                this.collisionManager.checkAll();
+            }
+        }, 1000 / 60);
+    }
+
+    updateAllObjects() {
+        const objects = [
+            this.character, ...this.level.enemies, ...this.throwableObjects,
+            ...this.level.neonSigns, ...this.level.vehicles.background,
+            ...this.level.vehicles.midground, ...this.level.collectableItems
+        ];
+        objects.forEach(obj => obj.updateState());
+
+        this.filterProjectiles();
+        this.filterEnemies();
+        this.updateCamera();
+        if (this.character.isDead()) this.resetBossBehavior();
+    }
 
     draw() {
         this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
-
-        // 1. STARTBILDSCHIRM
         if (!this.gameStarted) {
             this.uiManager.drawStartScreen();
             this.checkStartKey();
-            requestAnimationFrame(() => this.draw());
-            return;
+        } else {
+            this.renderGame();
+            this.renderUI();
+            this.checkEndStates();
         }
-
-        // --- Normaler Spiel-Draw-Code ---
-        this.drawParallaxLayer(0.2, this.backgroundLayers.back);
-        this.drawParallaxLayer(0.3, this.flyingVehicles.background);
-        this.drawParallaxLayer(0.6, this.backgroundLayers.middle);
-
-        this.ctx.save();
-        this.ctx.translate(this.camera_x, 0);
-        this.drawGameObjects();
-        this.ctx.restore();
-
-        this.addObjectsToMap(this.level.UIElements);
-        this.addObjectsToMap(this.level.statusIcons);
-
-        if (!this.showHelp) {
-            this.uiManager.drawHelpHint();
-        }
-
-        this.checkHelpToggle();
-        if (this.showHelp) {
-            this.uiManager.drawHelpOverlay();
-        }
-
-        // --- 2. ENDBILDSCHIRM LOGIK ---
-        if (this.character.energy <= 0) {
-            this.uiManager.drawEndScreen('LOSE');
-            this.checkStartKey(); // WICHTIG: Hier auf Enter prüfen!
-        } else if (this.bossIsDead()) {
-            this.uiManager.drawEndScreen('WIN');
-            this.checkStartKey(); // WICHTIG: Hier auf Enter prüfen!
-        }
-
         requestAnimationFrame(() => this.draw());
     }
 
-    checkHelpToggle() {
-        if (this.keyboard.KEY_H) {
-            if (!this.hKeyPressed) {
-                this.showHelp = !this.showHelp;
-                this.hKeyPressed = true;
-            }
-        } else {
-            this.hKeyPressed = false;
+    renderGame() {
+        this.drawParallaxLayer(0.2, this.level.backgrounds.back);
+        this.drawParallaxLayer(0.3, this.level.vehicles.background);
+        this.drawParallaxLayer(0.6, this.level.backgrounds.middle);
+        this.ctx.save();
+        this.ctx.translate(this.camera_x, 0);
+        this.drawWorldEntities();
+        this.ctx.restore();
+    }
+
+    drawWorldEntities() {
+        this.addObjectsToMap(this.level.vehicles.midground);
+        this.addObjectsToMap(this.level.backgrounds.foreground);
+        this.addObjectsToMap(this.level.collectableItems);
+        this.addObjectsToMap(this.level.neonSigns);
+        this.addObjectsToMap(this.level.enemies);
+        this.addToMap(this.character);
+        this.addObjectsToMap(this.throwableObjects);
+
+    }
+
+    renderUI() {
+        this.addObjectsToMap(this.level.UIElements);
+        this.addObjectsToMap(this.level.statusIcons);
+        this.checkHelpToggle();
+        this.showHelp ? this.uiManager.drawHelpOverlay() : this.uiManager.drawHelpHint();
+    }
+
+    checkEndStates() {
+        if (this.character.isDeadAnimationFinished()) {
+            this.uiManager.drawEndScreen('LOSE');
+            this.checkStartKey();
+        } else if (this.bossIsDead()) {
+            this.uiManager.drawEndScreen('WIN');
+            this.checkStartKey();
         }
     }
 
     checkStartKey() {
-        // Falls das Spiel läuft und jemand stirbt oder gewinnt:
-        if (this.character.energy <= 0 || this.bossIsDead()) {
-            if (this.keyboard.KEY_ENTER) {
-                this.restartGame();
-            }
-            return; // Verhindert, dass die normale Start-Logik greift
-        }
+        const isGameOver = this.character.energy <= 0 || this.bossIsDead();
+        if (isGameOver && this.keyboard.KEY_ENTER) location.reload();
+        else if (!this.gameStarted && (this.keyboard.KEY_ENTER || this.keyboard.LEFT_CLICK)) this.gameStarted = true;
+    }
 
-        // Die normale Logik für den allerersten Start
-        if (this.keyboard.KEY_ENTER || this.keyboard.LEFT_CLICK) {
-            this.gameStarted = true;
+    checkHelpToggle() {
+        if (this.keyboard.KEY_H && !this.hKeyPressed) {
+            this.showHelp = !this.showHelp;
+            this.hKeyPressed = true;
+        } else if (!this.keyboard.KEY_H) {
+            this.hKeyPressed = false;
         }
     }
 
@@ -142,156 +152,16 @@ class World {
         this.ctx.restore();
     }
 
-    drawGameObjects() {
-        this.addObjectsToMap(this.flyingVehicles.midground);
-        this.addObjectsToMap(this.backgroundLayers.foreground);
-        this.addObjectsToMap(this.level.collectableItems);
-        this.addObjectsToMap(this.neonSigns);
-        this.addToMap(this.character);
-        this.addObjectsToMap(this.enemies);
-        this.addObjectsToMap(this.throwableObjects);
-    }
-
-    run() {
-        setInterval(() => {
-            if (this.gameStarted && !this.showHelp && this.character.energy > 0) {
-                this.updateAllObjects();
-                this.checkCollisions();
-            }
-        }, 1000 / 60);
-    }
-
-    updateAllObjects() {
-        this.updateGameElements();
-        this.filterProjectiles();
-        this.filterEnemies();
-        this.updateCamera();
-
-        if (this.character.isDead()) {
-            this.resetBossBehavior();
-        }
-    }
-
-    updateGameElements() {
-        this.character.updateState();
-        this.enemies.forEach(enemy => enemy.updateState());
-        this.throwableObjects.forEach(objects => objects.updateState());
-        this.neonSigns.forEach(sign => sign.updateState());
-        this.flyingVehicles.background.forEach(vehicle => vehicle.updateState());
-        this.flyingVehicles.midground.forEach(vehicle => vehicle.updateState());
-        this.level.collectableItems.forEach(item => item.updateState());
-    }
-
-    checkCollisions() {
-        this.checkEnemyCollisions();
-        this.checkProjectileCollisions();
-        this.checkItemCollisions();
-    }
-
-    checkEnemyCollisions() {
-        const meleeDamage = ThrowableObject.BLUEPRINTS.MELEE.damage;
-        const meleeType = ThrowableObject.BLUEPRINTS.MELEE.type;
-
-        this.enemies.forEach((enemy) => {
-            if (!enemy.isDead() && this.character.isColliding(enemy)) {
-                this.character.hit(meleeDamage, meleeType);
-                this.healthBar.setPercentage(this.character.energy);
-            }
-        });
-    }
-
-    checkProjectileCollisions() {
-        this.throwableObjects.forEach((projectile) => {
-            if (projectile.hasHit) return;
-
-            if (projectile.isEnemy) {
-                this.checkHit(projectile, this.character);
-            } else {
-                this.enemies.forEach(enemy => {
-                    this.checkHit(projectile, enemy);
-                });
-            }
-        });
-    }
-
-    checkHit(projectile, target) {
-        if (projectile.isEnemy && target !== this.character) return;
-        if (!projectile.isEnemy && target === this.character) return;
-
-        if (!projectile.hasHit && !target.isDead() && projectile.isColliding(target)) {
-            if (projectile instanceof BossBomb) {
-                if (!projectile.hasExploded) {
-                    target.hit(projectile.damage, projectile.damageType);
-                    this.healthBar.setPercentage(this.character.energy);
-                    projectile.explode();
-                }
-            } else {
-                projectile.hasHit = true;
-                target.hit(projectile.damage, projectile.damageType);
-                if (target === this.character) {
-                    this.healthBar.setPercentage(this.character.energy);
-                }
-            }
-        }
-    }
-
-    checkEnemyProjectileHitPlayer(projectile) {
-        if (this.character.isColliding(projectile)) {
-            projectile.hasHit = true;
-            this.character.hit(projectile.damage, projectile.damageType);
-            this.healthBar.setPercentage(this.character.energy);
-
-            if (projectile instanceof BossBomb && !projectile.hasExploded) {
-                projectile.explode();
-            }
-        }
-    }
-
-    checkBossBombCollision(bomb) {
-        if (this.character.isColliding(bomb)) {
-            this.character.hit(bomb.damage, bomb.damageType);
-            this.healthBar.setPercentage(this.character.energy);
-            if (typeof bomb.explode === 'function' && !bomb.hasExploded) {
-                bomb.explode();
-            }
-        }
-    }
-
-    checkPlayerProjectileHitEnemy(projectile) {
-        this.enemies.forEach((enemy) => {
-            if (!projectile.hasHit && !enemy.isDead() && projectile.isColliding(enemy)) {
-                projectile.hasHit = true;
-                enemy.hit(projectile.damage, projectile.damageType);
-            }
-        });
-    }
-
-    checkItemCollisions() {
-        this.level.collectableItems.forEach((item, index) => {
-            if (this.character.isColliding(item)) {
-                this.handleItemPickup(item, index);
-            }
-        });
-    }
-
-    handleItemPickup(item, index) {
-        if (item instanceof PlasmaCore || item instanceof PowerCell) {
-            this.character.plasma = Math.min(100, this.character.plasma + 40);
-            this.plasmaBar.setPercentage(this.character.plasma);
-        }
-        if (item instanceof Mediapack) {
-            this.character.energy = Math.min(100, this.character.energy + 40);
-            this.healthBar.setPercentage(this.character.energy);
-        }
-
-        this.level.collectableItems.splice(index, 1);
+    updateCamera() {
+        let target_x = Math.max(-(this.level.level_end_x - this.canvas.width), Math.min(0, -this.character.x + 100));
+        let shake = this.isShaking ? (Math.random() - 0.5) * 20 : 0;
+        this.camera_x = target_x + shake;
     }
 
     filterProjectiles() {
-        this.throwableObjects = this.throwableObjects.filter(obj => {
-            let isInsideX = obj.x > this.character.x - 600 && obj.x < this.character.x + 600;
-            return isInsideX && !obj.hasHit;
-        });
+        this.throwableObjects = this.throwableObjects.filter(obj =>
+            Math.abs(obj.x - this.character.x) < 600 && !obj.hasHit
+        );
     }
 
     filterEnemies() {
@@ -299,56 +169,37 @@ class World {
         this.enemies = this.level.enemies;
     }
 
-    triggerCameraShake() {
-        if (this.isShaking) return;
-        this.isShaking = true;
-        setTimeout(() => {
-            this.isShaking = false;
-        }, 300);
-    }
-
-    updateCamera() {
-        let target_camera_x = Math.max(-(this.level.level_end_x - this.canvas.width), Math.min(0, -this.character.x + 100));
-
-        if (this.isShaking) {
-            let shakeOffset = (Math.random() - 0.5) * 20;
-            this.camera_x = target_camera_x + shakeOffset;
-        } else {
-            this.camera_x = target_camera_x;
-        }
-    }
-
-    addToMap(mo) {
-        mo.draw(this.ctx);
-    }
-
-    addObjectsToMap(objects) {
-        objects.forEach(obj => this.addToMap(obj));
-    }
-
     bossIsDead() {
-        let boss = this.enemies.find(e => e instanceof Endboss);
+        let boss = this.level.enemies.find(e => e instanceof Endboss);
         return boss && boss.isDead();
     }
 
     resetBossBehavior() {
-        this.enemies.forEach(enemy => {
-            if (enemy instanceof Endboss) {
-                if (typeof enemy.performPatrol === 'function') {
-                    enemy.isAnimatingOnce = false;
-                }
-            }
-        });
+        this.level.enemies.forEach(e => { if (e instanceof Endboss) e.isAnimatingOnce = false; });
     }
 
-    restartGame() {
-        this.character.energy = 100;
-        this.character.x = 100;
-        this.character.plasma = 100;
-        this.character.ammo = 20;
-        this.camera_x = 0;
-        this.healthBar.setPercentage(100);
-        this.plasmaBar.setPercentage(100);
-        location.reload();
+    addToMap(mo) { mo.draw(this.ctx); }
+    addObjectsToMap(objs) { objs.forEach(obj => this.addToMap(obj)); }
+
+    spawnTraffic(count) {
+        for (let i = 0; i < count; i++) {
+            let x = Math.random() * this.level.level_end_x;
+            let y = 20 + Math.random() * 180;
+            let speed = 0.5 + Math.random() * 2;
+            let direction = Math.random() > 0.5 ? 1 : -1;
+
+            let type = Math.random();
+            let v;
+
+            if (type < 0.3) {
+                v = new Police(x, y, speed, direction, this, 0.3);
+            } else if (type < 0.6) {
+                v = new Drone(x, y, speed, direction, this, 0.3);
+            } else {
+                v = new Truck(x, y, speed, direction, this, 0.3);
+            }
+
+            this.level.vehicles.background.push(v);
+        }
     }
 }
