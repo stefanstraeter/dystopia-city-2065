7ee35943f75ab2e5f998697e11de977ca83265f7
Stefan Sträter <stefan.straeter@gmail.com>
2026-03-20 12:28:00 +0100

    feat: implement AudioManager with volume control and autoplay handling

--- models/audio-manager.class.js
+++ models/audio-manager.class.js
@@ -1,15 +1,32 @@
 class AudioManager {
     constructor() {
         this.sounds = {
-            background: new Audio('./assets/audio/cyberpunk-street.mp3'),
+            background: new Audio('./assets/audio/cyberpunk.mp3'),
+            plasma: new Audio('./assets/audio/plasma.wav'),
+            collect: new Audio('./assets/audio/collect.wav'),
+            explosion: new Audio('./assets/audio/explosion.wav')
         };
-
         this.initSettings();
     }
 
     initSettings() {
         this.sounds.background.loop = true;
-        this.sounds.background.volume = 0.4;
+        this.sounds.background.volume = 0.2;
+        if (this.sounds.plasma) {
+            this.sounds.plasma.volume = 0.05;
+        }
+        if (this.sounds.explosion) {
+            this.sounds.explosion.volume = 0.3;
+        }
+        if (this.sounds.collect) {
+            this.sounds.collect.volume = 0.3;
+        }
+    }
+
+    setVolume(name, volume) {
+        if (this.sounds[name]) {
+            this.sounds[name].volume = Math.max(0, Math.min(1, volume));
+        }
     }
 
     play(name) {
@@ -18,10 +35,8 @@ class AudioManager {
         if (name !== 'background') {
             s.currentTime = 0;
         }
-
         s.play().catch(e => console.warn("Audio-Autoplay blockiert:", e));
     }
-
     stop(name) {
         if (this.sounds[name]) {
             this.sounds[name].pause();
