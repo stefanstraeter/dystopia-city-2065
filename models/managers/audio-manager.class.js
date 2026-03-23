b4a9a4333a83b2e287d03d2372bb2ab0315784f7
Stefan Sträter <stefan.straeter@gmail.com>
2026-03-23 15:55:49 +0100

    fix: balance audio levels of music and sfx

--- models/managers/audio-manager.class.js
+++ models/managers/audio-manager.class.js
@@ -23,10 +23,10 @@ class AudioManager {
      */
     initSettings() {
         this.sounds.background.loop = true;
-        this.sounds.background.volume = 0.3;
-        this.sounds.plasma.volume = 0.1;
-        this.sounds.explosion.volume = 0.2;
-        this.sounds.collect.volume = 0.4;
+        this.sounds.background.volume = 0.1;
+        this.sounds.plasma.volume = 0.05;
+        this.sounds.explosion.volume = 0.1;
+        this.sounds.collect.volume = 0.2;
     }
 
     /**
